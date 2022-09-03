d40074d90d4f67f65b973af0eac6e9e9115215e4
Killian Lanoe <naughtywarrior404@gmail.com>
2022-09-03 14:17:59 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const path = require('path')
-const helmet = require('helmet')
 const app = express();
 require('dotenv').config()
 
 const userRoutes = require('./routes/user')
 const productRoutes = require('./routes/product')
 
-app.use(express.json());
-app.use(helmet())
 
 mongoose.connect('mongodb+srv://' + process.env.MDB_User + ':' + process.env.MDB_Pass + '@cluster0.xrnytdj.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -19,12 +16,15 @@ mongoose.connect('mongodb+srv://' + process.env.MDB_User + ':' + process.env.MDB
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
 
 
 app.use('/api/sauces', productRoutes);
